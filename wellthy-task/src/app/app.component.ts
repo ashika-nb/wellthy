2870dcc5e9a9415e43fd190f77ad19f0c61d1a51
Ashika NB <ashikanb@Ashikas-MacBook-Air.local>
2022-04-22 23:46:47 +0530

    layout/ basic styles

---
+++ wellthy-task/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'wellthy';
+  number = new FormControl();
+  
+
+}
